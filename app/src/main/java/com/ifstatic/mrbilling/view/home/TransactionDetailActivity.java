5685bd6bc7159ffcaa3845418cb9379fcdb0ccfb
Vanshika <vanshikarathod14@gmail.com>
2023-06-05 16:40:36 +0530

    Transaction Detail Page Added

--- app/src/main/java/com/ifstatic/mrbilling/view/home/TransactionDetailActivity.java
+++ app/src/main/java/com/ifstatic/mrbilling/view/home/TransactionDetailActivity.java
@@ -3,14 +3,33 @@ package com.ifstatic.mrbilling.view.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.ifstatic.mrbilling.R;
+import com.ifstatic.mrbilling.databinding.ActivityTransactionDetailBinding;
+import com.ifstatic.mrbilling.databinding.ActivityViewAllTransactionBinding;
 
 public class TransactionDetailActivity extends AppCompatActivity {
 
+    private ActivityTransactionDetailBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transaction_detail);
+        binding = ActivityTransactionDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initListeners();
+    }
+
+    private void initListeners() {
+
+        binding.header.titleTextView.setText("Transaction Details");
+
+        binding.header.backImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
